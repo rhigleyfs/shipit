738e1bd307e317b88ec3dac7ecaa1bcb8bc73c60
Rachel Higley <rhigley@fullsail.com>
2020-04-02 17:40:35 -0400

    Stop watching

--- reactjs/src/pages/tag/index.js
+++ reactjs/src/pages/tag/index.js
@@ -8,6 +8,7 @@ import { Button } from 'bloomer';
 import PostList from '../../components/postList';
 import examplePosts from '../../exampleData/posts.json';
 import tagData from '../../exampleData/tags.json';
+import userData from '../../exampleData/users';
 
 class Tag extends Component {
   componentDidMount() {
@@ -30,15 +31,36 @@ class Tag extends Component {
     addToWatched(tagId);
   };
 
+  onStopWatchClick = () => {
+    const {
+      match: {
+        params: { tagId },
+      },
+      removeFromWatched,
+    } = this.props;
+    removeFromWatched(tagId);
+  };
+
   render() {
-    const { posts, tag } = this.props;
+    const { posts, tag, user } = this.props;
+    const alreadyWatching = user.tags.find((t) => t.id === tag.id);
     return (
       <>
         <h1 className={styles.heading}>
           {tag.name}
-          <Button className={styles.button} onClick={this.onWatchClick}>
-            Watch Tag
-          </Button>
+          {!alreadyWatching && (
+            <Button className={styles.button} onClick={this.onWatchClick}>
+              Watch Tag
+            </Button>
+          )}
+          {alreadyWatching && (
+            <Button
+              className={styles.stopButton}
+              onClick={this.onStopWatchClick}
+            >
+              Stop Watching
+            </Button>
+          )}
         </h1>
 
         <PostList posts={posts} />
@@ -51,9 +73,13 @@ Tag.propTypes = {
   addToWatched: PropTypes.func,
   fetchPosts: PropTypes.func,
   posts: PropTypes.arrayOf(PropTypes.object),
+  removeFromWatched: PropTypes.func,
   tag: PropTypes.shape({
     id: PropTypes.string,
     name: PropTypes.string,
+  }),
+  user: PropTypes.shape({
+    tags: PropTypes.arrayOf(PropTypes.object),
   }),
 };
 
@@ -63,7 +89,9 @@ Tag.defaultProps = {
   posts: examplePosts.filter(
     (post) => post.tags.filter((t) => t.id === tagData[3].id).length
   ),
+  removeFromWatched: () => {},
   tag: tagData[3],
+  user: userData[3],
 };
 
 export default Tag;
